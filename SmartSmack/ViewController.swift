90e84d08f0a72ad73b51d53c52151fb289f5706e
Jose Trinidad <internick18@gmail.com>
2019-01-22 18:23:55 -0600

    Initial Commit

---
+++ SmartSmack/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SmartSmack
+//
+//  Created by Admin on 22/01/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
