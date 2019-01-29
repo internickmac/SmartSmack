3af00cb69935bcea53f78527017af7aa34827f7b
Jose Trinidad <internick18@gmail.com>
2019-01-28 19:58:40 -0600

    Login UI Design

---
+++ SmartSmack/Controllers/LoginVC.swift
@@ -0,0 +1,23 @@
+//
+//  LoginVC.swift
+//  SmartSmack
+//
+//  Created by Admin on 28/01/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+}
---
+++ SmartSmack/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  SmartSmack
+//
+//  Created by Admin on 28/01/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import Foundation
+
+//Segues
+let TO_LOGIN = "toLogin"
