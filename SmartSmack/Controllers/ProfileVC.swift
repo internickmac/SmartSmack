11d3d43e0dd758ce8eb79540e4364851849847ef
Jose Trinidad <internick18@gmail.com>
2019-02-20 19:55:20 -0600

    Commit added Notification for login and chanel page an restore mlabs data in te Xib controller

---
+++ SmartSmack/Controllers/ProfileVC.swift
@@ -0,0 +1,63 @@
+//
+//  ProfileVC.swift
+//  SmartSmack
+//
+//  Created by Admin on 19/02/19.
+//  Copyright © 2019 Admin. All rights reserved.
+//
+
+import UIKit
+
+class ProfileVC: UIViewController {
+
+    @IBOutlet weak var userEmail: UILabel!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var userName: UILabel!
+    @IBOutlet weak var bgView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+
+    @IBAction func logoutPressed(_ sender: Any) {
+        UserDataService.instance.logoutUser()
+        NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_GHANGE, object: nil)
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func closeModalPressed(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func setupView() {
+        userName.text = UserDataService.instance.name
+        userEmail.text = UserDataService.instance.email
+        profileImage.image = UIImage(named: UserDataService.instance.avatarName)
+        profileImage.backgroundColor = UserDataService.instance.returnUIColor(components: UserDataService.instance.avatarColor)
+        let closeTouch = UITapGestureRecognizer(target: self, action: #selector(ProfileVC.closeTap(_:)))
+        bgView.addGestureRecognizer(closeTouch)
+        
+    }
+    
+    @objc func closeTap(_ recognizer: UITapGestureRecognizer) {
+        dismiss(animated: true, completion: nil)
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
